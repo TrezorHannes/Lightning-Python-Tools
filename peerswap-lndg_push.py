52e39c1b7dd081a43d6867596b60c3e2ed033968
TrezorHannes <hakezuba@proton.me>
2024-01-25 23:01:12 +0100

    Working file but dependency on LNDg feature to allow notes-update via API

--- peerswap-lndg_push.py
+++ peerswap-lndg_push.py
@@ -1,67 +1,96 @@
-## This needs further debug, not working yet 
+## This script is still missing a serializer for notes in LNDg being updated via API
+## Until this is implemented, it's not going to work for automated updates
+## However, you can uncomment the debug-entries in the main() function to get a quick overview for manual copy & paste
 
 import datetime
 import os
 import requests
 import json
 import subprocess
+import logging  # For more structured debugging
 
 # Define the command to get peers information
 pscli_command = ['pscli', 'listpeers']
 
 # LNDg API credentials and endpoints
 username = 'lndg-admin'
-password = '${{ secrets.PASSWORD }}'
-get_api_url = 'http://debian-nuc.local:8889/api/channels?limit=500'
-update_api_url = 'http://debian-nuc.local:8889/api/chanpolicy/'
+password = '{{ LNDG-PASSWORD }}'
+get_api_url = 'http://localhost:8889/api/channels'
+update_api_url = 'http://localhost:8889/api/chanpolicy/'
 
 # File path for the log file
 log_file_path = os.path.expanduser('~/peerswap-LNDg_changes.log')
 
-'''
-# Function to execute the command and return JSON output
-def get_pscli_listpeers_output():
-    result = subprocess.run(['pscli', 'listpeers'], stdout=subprocess.PIPE)
-    return json.loads(result.stdout)
-'''
+# Logfile definition
+logging.basicConfig(filename=log_file_path, level=logging.DEBUG) 
+
+# Alt function to get the output of 'lncli listchannels'
+def get_lncli_listchannels_output():
+    result = subprocess.run(['lncli', 'listchannels'], stdout=subprocess.PIPE)
+    channels_data = json.loads(result.stdout)
+    return channels_data['channels']
+
+# Alt function to match alias using channel-id
+def find_alias_by_chan_id(chan_id):
+    channels_data = get_lncli_listchannels_output()
+    for channel in channels_data:
+        if channel['chan_id'] == chan_id:
+            return channel['peer_alias']
+    return None
 
 # Function to get peers information from peerswap
 def get_peerswap_info():
     try:
-        # Run the pscli command and capture the output
+        # Run the pscli command and capture the output - needs to be in path
         result = subprocess.run(pscli_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        try:
+            peers_info = json.loads(result.stdout)
+        except json.JSONDecodeError as e:
+            print(f"Error parsing JSON: {e}")
+            return None 
         if result.returncode == 0:
             # Parse the JSON output
             peers_info = json.loads(result.stdout)
             # Extract the required information
             formatted_info = []
-            for peer in peers_info:
+            for peer in peers_info['peers']:
+                if peer.get('channels'):  # Check if the 'channels' list exists
+                    channel_id = peer['channels'][0]['channel_id'] 
+                else:
+                    channel_id = ""  # when there's no channel
+
                 swaps_allowed = peer['swaps_allowed']
                 supported_assets = ', '.join(peer['supported_assets'])
                 swaps_out = sum(int(ch['swaps_out']) for ch in [peer['as_sender'], peer['as_receiver']])
                 swaps_in = sum(int(ch['swaps_in']) for ch in [peer['as_sender'], peer['as_receiver']])
                 paid_fee = int(peer['paid_fee'])
 
-                notes = (
+                new_notes = (
                     f"Swaps Allowed: {swaps_allowed}\n"
                     f"Assets Allowed: {supported_assets}\n"
                     f"SUM of Swap-Outs: {swaps_out}\n"
                     f"SUM of Swap-Ins: {swaps_in}\n"
                     f"Paid fee: {paid_fee}"
                 )
-                formatted_info.append((peer['channels'][0]['channel_id'], notes))
+                
+                formatted_info.append((channel_id, new_notes))  # Append individual peer objects
             return formatted_info
         else:
-            print(f"Error running pscli: {result.stderr}")
+            logging.error(f"Error running pscli: {result.stderr}")
             return None
     except Exception as e:
         print(f"Error: {e}")
+        logging.error(f"Error: {e}")
+
         return None
 
+def get_current_timestamp():
+    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
 # Function to get current notes from LNDg API
-def get_current_notes(chan_id):
+def get_current_notes(channel_id):
     # Use the base API URL and append the specific channel ID
-    api_url = f"{get_api_url}/{chan_id}/"
+    api_url = f"{get_api_url}/{channel_id}/"
 
     try:
         # Make the API request with authentication
@@ -71,58 +100,74 @@ def get_current_notes(chan_id):
         if response.status_code == 200:
             data = response.json()
             # Extract the 'notes' field from the JSON response
+            # logging.debug(f"Data type: {type(data)}, value: {data}")
             notes = data.get('notes', '')
             return notes
         else:
-            print(f"API request for channel {chan_id} failed with status code: {response.status_code}")
+            logging.error(f"API request for channel {channel_id} failed with status code: {response.status_code}")
             return None
 
     except Exception as e:
-        print(f"Error retrieving notes for channel {chan_id}: {e}")
+        logging.error(f"Error retrieving notes for channel {channel_id}: {e}")
         return None
 
 
 # Function to update notes on LNDg API
-def update_notes(chan_id, notes):
+def update_notes(channel_id, notes):
     payload = {
-        "chan_id": chan_id,
+        "chan_id": channel_id,
         "notes": notes
     }
     try:
-        # Make a PUT request to the LNDg API to update the notes
-        response = requests.put(update_api_url, json=payload, auth=(username, password))
+        # Make a POST request to the LNDg API to update the notes
+        response = requests.post(update_api_url, json=payload, auth=(username, password))
 
-        # Get the current timestamp
-        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
+        timestamp = get_current_timestamp()
+        logging.debug(f"Channel-ID: {channel_id}")
+        logging.debug(f"Payload: {payload}")
+        logging.debug(f"API Response: {response.text}")
+        logging.debug(f"API Status Code: {response.status_code}")
+        logging.debug(f"Timestamp: {timestamp}")
         if response.status_code == 200:
             # Log the changes
             with open(log_file_path, 'a') as log_file:
-                log_file.write(f"{timestamp}: Updated notes for channel {chan_id}\n")
-            print(f"{timestamp}: Updated notes for channel {chan_id}")
+                log_file.write(f"{timestamp}: Updated notes for channel {channel_id}\n")
+            print(f"{timestamp}: Updated notes for channel {channel_id}")
         else:
-            print(f"{timestamp}: Failed to update notes for channel {chan_id}: Status Code {response.status_code}")
+            logging.error(f"{timestamp}: Failed to update notes for channel {channel_id}: Status Code {response.status_code}")
 
     except Exception as e:
-        print(f"Error updating notes for channel {chan_id}: {e}")
+        logging.error(f"Error updating notes for channel {channel_id}: {e}")
 
 # Main function
 def main():
+    channels_data = get_lncli_listchannels_output()
     peers_info = get_peerswap_info()
     if peers_info:
-        for chan_id, new_notes in peers_info:
-            current_notes = get_current_notes(chan_id)
-            print(f"Channel-ID: {chan_id}")
-            print(f"Notes old: {current_notes}")
-            print(f"Notes new: {new_notes}")
-            overwrite = input("Do you want to overwrite the existing notes? (y/n): ")
-            if overwrite.lower() == 'y':
-                #update_notes(chan_id, new_notes)
-                # Instead of updating, print the API URL and the new notes for debugging
-                debug_api_url = f"{update_api_url}{chan_id}/"
-                print(f"Debug: Would PUT to {debug_api_url} with notes: {new_notes}")
+        for channel_id, new_notes in peers_info:
+            alias = find_alias_by_chan_id(channel_id) 
+            current_notes = get_current_notes(channel_id)
+            if not current_notes:
+                print("============================================================================")
+                print(f"No existing notes stored in LNDg for {alias} Channel {channel_id}.")
+                print(f"Overwriting with new notes:\n{new_notes}.")
+                # update_notes(channel_id, new_notes)
             else:
-                print("Skipping update for this channel.")
+                print("============================================================================")
+                print(f"Existing notes stored in LNDg for {alias} Channel {channel_id}:\n{current_notes}")
+                action = input("Do you want to overwrite the existing notes (o) or append the new notes (a)? (o/a): ")
+                if action.lower() == 'o':
+                    print(f"Overwriting the existing notes with new notes:\n{new_notes}.")
+                    # debug_api_url = f"{update_api_url}{channel_id}/"
+                    # print(f"Debug: Would PUT {channel_id} to {debug_api_url} with notes:\n{new_notes}")
+                    update_notes(channel_id, new_notes)
+                elif action.lower() == 'a':
+                    print(f"Appending the existing notes with new notes:\n{current_notes}\n{new_notes}.")
+                    # debug_api_url = f"{update_api_url}{channel_id}/"
+                    # print(f"Debug: Would PUT {channel_id} to {debug_api_url} with notes:\n{current_notes}\n{new_notes}")
+                    update_notes(channel_id, current_notes + new_notes)
+                else:
+                    print(f"Invalid action. Skipping update for this channel.")
 
 if __name__ == "__main__":
     main()
